fixed bug in update function regarding logprobs and oldlogprob shapes

source/ppo_agent.py
@@ -152,18 +152,18 @@ class ActorCritic(nn.Module):
         return action_list
     
     def act_max(self, state, memory, num_agents):
-        with torch.no_grad():
-            state = torch.from_numpy(state).float().to(device)
-            action_probs = self.action_layer(state)
-            dist = Categorical(action_probs)
-            action = dist.sample()
-        
-            action_list = []
-            for agent_index in range(num_agents):
-                memory.states.append(state[agent_index])
-                memory.actions.append(action[agent_index])
-                memory.logprobs.append(dist.log_prob(action[agent_index])[agent_index])
-                action_list.append(action[agent_index].item())
+#        with torch.no_grad():
+        state = torch.from_numpy(state).float().to(device)
+        action_probs = self.action_layer(state)
+        dist = Categorical(action_probs)
+        action = dist.sample()
+    
+        action_list = []
+        for agent_index in range(num_agents):
+            memory.states.append(state[agent_index])
+            memory.actions.append(action[agent_index])
+            memory.logprobs.append(dist.log_prob(action[agent_index])[agent_index])
+            action_list.append(action[agent_index].item())
         return action_list
     
     def evaluate(self, state, action):
@@ -244,7 +244,7 @@ class PPO:
                 # Evaluating old actions and values :
                 logprobs, state_values, dist_entropy = self.policy.evaluate(old_states, old_actions)
                 # Finding the ratio (pi_theta / pi_theta__old):
-                ratios = torch.exp(logprobs - old_logprobs.detach())
+                ratios = torch.exp(logprobs.view(-1,1) - old_logprobs.view(-1,1).detach())
                     
                 # Finding Surrogate Loss:
                 advantages = rewards - state_values.detach()